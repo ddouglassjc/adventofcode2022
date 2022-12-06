943d245e12d2ad78ed32a81ffd1d5dd363e6145f
Drew Douglass <drew.douglass@jumpcloud.com>
2022-12-06 11:10:02 -0500

    trying to get part 2 working

--- day1.py
+++ day1.py
@@ -3,6 +3,7 @@ import os
 raw_data_filename = "day1.txt"
 raw_data = os.path.join(os.path.dirname(__file__), raw_data_filename)
 big_chungus, current_elf = 0, 0
+top_elfs = []
 
 with open(raw_data) as file:
     for line in file:
@@ -12,5 +13,12 @@ with open(raw_data) as file:
         else:
             if current_elf > big_chungus: 
                 big_chungus = current_elf
+                top_elfs.append(big_chungus)
             current_elf = 0 
-print(f"Big chungus is the snack plug with {big_chungus} kcals.")
+    print(top_elfs)
+    top_elfs.sort()
+    print(top_elfs)
+    print(sum(top_elfs))
+
+
+#print(f"Big chungus is the snack plug with {big_chungus} kcals.") #part 1 answer
