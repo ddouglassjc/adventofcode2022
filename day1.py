479b647efca1446cb71b17397d0e81b7f44243b8
Drew Douglass <drew.douglass@jumpcloud.com>
2022-12-06 19:50:19 -0500

    Solved part 2.

--- day1.py
+++ day1.py
@@ -3,23 +3,19 @@ import os
 raw_data_filename = "day1.txt"
 raw_data = os.path.join(os.path.dirname(__file__), raw_data_filename)
 big_chungus, current_elf = 0, 0
-top_elfs = []
-top_three = ""
+all_elfs = []
 with open(raw_data) as file:
     for line in file:
         strip_it = line.strip()
         if strip_it.isdigit():
             current_elf += int(strip_it)
         else:
+            all_elfs.append(current_elf)
             if current_elf > big_chungus: 
                 big_chungus = current_elf
-                top_elfs.append(big_chungus)
             current_elf = 0 
-    print(top_elfs)
-    top_elfs.sort(reverse=True)
-    top_three = top_elfs[:3]
-    print(top_three)
-    print(sum(top_three))
+all_elfs.sort()
+print(sum(all_elfs[-3:])) #part 2 answer
 
 
-#print(f"Big chungus is the snack plug with {big_chungus} kcals.") #part 1 answer
+print(f"Big chungus is the snack plug with {big_chungus} kcals.") #part 1 answer
